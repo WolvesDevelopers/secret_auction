d751c6e8834e78443f15e1bbb165c055c2174637
Oswaldo Yanez <oswaldo.business31@gmail.com>
2022-07-14 13:39:22 -0400

    add: entry point.

--- secret_auction.py
+++ secret_auction.py
@@ -1,6 +1,7 @@
 from art import logo
 import os
-#HINT: You can call clear() to clear the output in the console.
+
+
 print(logo)
 print("Welcome to the secret auction program.")
 participants = []
@@ -12,31 +13,37 @@ def add_new_participants(participant_name, participant_bid):
         "bid" : participant_bid
     }
     return participants.append(my_dict)
-    
 
-access = True
-while access:
-  mayor = 0
-  name = input("What is your name ?: ")
-  bid = int(input("What's your bid?: $"))
-  add_new_participants(name,bid)
-  level2 = True
-  while level2:
-    should_continue  = input("Are there any other bidders? Type 'yes' or 'no'.\n")
-    if (should_continue  == "no"):
-      for participant in range(len(participants)):
+def run():
+  access = True
+  
+  while access:
+    mayor = 0
+    name = input("What is your name ?: ")
+    bid = int(input("What's your bid?: $"))
+    add_new_participants(name,bid)
+    level2 = True
+    
+    while level2:
+      should_continue  = input("Are there any other bidders? Type 'yes' or 'no'.\n")
+      if (should_continue  == "no"):
+        for participant in range(len(participants)):      
+          if(participants[participant]["bid"] > mayor):
+            mayor = participants[participant]["bid"]
+            winner = participants[participant]["name"]
+        print(f"the winner is {winner} with a bid of ${mayor}")
         
-        if(participants[participant]["bid"] > mayor):
-          mayor = participants[participant]["bid"]
-          winner = participants[participant]["name"]
+        access = False
+        level2 = False
 
-      print(f"the winner is {winner} with a bid of ${mayor}")
-      access = False
-      level2 = False
+      elif(should_continue  == "yes"):
+        os.system ("clear") 
+        level2 = False
+      
+      else:
+        print("invalid option.")    
 
-    elif(should_continue  == "yes"):
-      os.system ("clear") 
-      level2 = False
-    
-    else:
-      print("invalid option.")
+
+if __name__== "__main__":
+  run()
+
